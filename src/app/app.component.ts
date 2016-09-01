9dd94e135524bab0230ef5b8de359095fd9a3a6f
Stefan Aerts <aerts.stefan@icloud.com>
2016-09-01 12:40:55 +0200

    test version with FirebaseListObservable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,116 @@
 import { Component } from '@angular/core';
-
+ import { AngularFire, FirebaseListObservable} from 'angularfire2';
+// import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
+ templateUrl: 'app.component.html',
+ // template: `
+ // <h1>{{ items | async | json }}</h1>
+//   `,
+
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+ // title = 'app werkt';
+ // isAuth = false;
+//  authColor = 'warn';
+//  user = {};
+// item: FirebaseObjectObservable<any>;
+items: FirebaseListObservable<any>;
+
+constructor(
+    af: AngularFire
+  ) {
+ // this.item = af.database.object('/item');
+  // this.item = af.database.object('/item', { preserveSnapshot: true });
+  this.items = af.database.list('/messages');
+  // const promise = this.items.push({ name: "newlist" });
+  // promise
+  // .then(_ => console.log('success'))
+ // .catch(err => console.log(err, 'You do not have access!'));
+ // this.items.subscribe(snapshot => {
+ // console.log(snapshot.key)
+ // console.log(snapshot.val())
+ // });
+ //   this.af.auth.subscribe(
+ //     user => this._changeState(user),
+ //     error => console.trace(error)
+ //   );
+}
+ addItem(newName: string) {
+    this.items.push({ text: newName });
+  }
+  updateItem(key: string, newText: string) {
+    this.items.update(key, { text: newText });
+  }
+  deleteItem(key: string) {
+    this.items.remove(key);
+  }
+  deleteEverything() {
+    this.items.remove();
+  }
+/* save(newName: string) {
+    this.item.set({ name: newName });
+  }
+  update(newSize: string) {
+    this.item.update({ size: newSize });
+  }
+  delete() {
+    const promise = this.item.remove();
+    promise
+      .then(_ => console.log('success'))
+      .catch(err => console.log('no permissions to remove'));
+  }
+  */
+/*
+login() {
+  this.af.auth.login({
+    provider: AuthProviders.Google
+  });
+}
+*/
+
+/*login(from: string) {
+    this.af.auth.login({
+      provider: this._getProvider(from)
+    });
+  }
+  logout() {
+    this.af.auth.logout();
+  }
+
+private _changeState(user: any = null) {
+    if (user) {
+      this.isAuth = true;
+      this.authColor = 'primary';
+      this.user = this._getUserInfo(user);
+    } else {
+      this.isAuth = false;
+      this.authColor = 'warn';
+      this.user = {};
+    }
+}
+
+private _getUserInfo(user: any): any {
+    if (!user) {
+      return {};
+    }
+    let data = user.auth.providerData[0];
+    return {
+      name: data.displayName,
+      avatar: data.photoURL,
+      email: data.email,
+      provider: data.providerId
+    };
+  }
+
+private _getProvider(from: string) {
+    switch (from) {
+      case 'twitter': return AuthProviders.Twitter;
+      case 'facebook': return AuthProviders.Facebook;
+      case 'github': return AuthProviders.Github;
+      case 'google': return AuthProviders.Google;
+    }
+  }
+  */
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,42 @@
+// CORE DEPS
+import {  NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ApplicationRef } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AngularFireModule } from 'angularfire2';
+
+// APP
+import { AppComponent} from './app.component';
+
+ // import { CommonModule } from '@angular/common';
+ // import { FormsModule } from '@angular/forms';
+ // import * as firebase from 'firebase';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyA9wBgcRsLyoQQiaNb4HgFxnBlos-HWKDA',
+  authDomain: 'pokerapp-6892e.firebaseapp.com',
+  databaseURL: 'https://pokerapp-6892e.firebaseio.com',
+  storageBucket: 'pokerapp-6892e.appspot.com'
+};
 
 @NgModule({
+   imports: [
+    BrowserModule,
+   // CommonModule,
+   // FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig)
+     /*,
+      {
+        method: AuthMethods.Popup,
+        // method: AuthMethods.Redirect
+      })
+    */
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    CommonModule,
-    FormsModule
-  ],
-  providers: [],
-  entryComponents: [AppComponent],
+ // providers: [],
+ // entryComponents: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {
